fc1d6e862a23998fd100940641e1a93663fba119
Gabrilinsky <gabrielnaranjo@liceovvh.cl>
2026-04-09 12:58:01 -0400

    Tarea Lista

---
+++ 01_Introducción/1_Hola_mundo/static/js/script.js
@@ -0,0 +1,14 @@
+// Prueba de conexión con js
+console.log("Hola Mundo, desde archivo externo!...");
+
+// 1) Hola mundo con variable y const.
+const saludo = "Hola a tdos,";
+let destinatario = "estudiantes"
+
+// Impresión de saludpo concatenando con (+).
+console.log(saludo + "Para los" + destinatario);
+
+// 2) Saludar a una persona ingresada por teclado.
+let nombre = prompt("Ingrese su nombre: ");
+// Imprimir un nombre ingresado con un prompt
+console.log("Hola," + nombre);
---
+++ 02_Tipos_De_Datos/1-Tipo_Texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexión con js correcta...");
+/* 
+Dato: Tipo Texto(string)
+Concatenación de texto con el signo +
+Podemos unir texto y variables
+*/
+const nombre =  "Gabriel";
+const apellido = "Naranjo";
+//Unimos ambas constantes con un texto extra.
+console.log("Hola, mi nombre es:" + nombre + " " + apellido);
