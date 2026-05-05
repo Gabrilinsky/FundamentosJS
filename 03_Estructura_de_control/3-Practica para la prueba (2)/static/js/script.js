00f8dc35a978c6a221e1c03acae15b46217a5e94
Gabrilinsky <gabrielnaranjo@liceovvh.cl>
2026-05-05 09:53:44 -0400

    Tarea lista

--- 03_Estructura_de_control/3-Practica para la prueba (2)/static/js/script.js
+++ 03_Estructura_de_control/3-Practica para la prueba (2)/static/js/script.js
@@ -106,5 +106,97 @@ alert(`El precio del cliente frecuente de ${nombre2.toUpperCase()} y su precio e
 ${precio2} el descuento del 10% es: $${precio2 * 0.1} 
 pero con el descuento es de: ${precio - (precio * 0.10)}`);
 alert(`El precio del cliente normal de ${nombre3.toUpperCase()} y su precio es:
-${precio3} sin el descuento`)
+${precio3} sin el descuento seria el mismo precio del cliente normal y es
+$${precio3}.`)
 }
+//Ejercicio 17: Análisis de frase con puntuación
+//Crear una función que almacene una frase y un puntaje numérico. Luego:
+//Convertir la frase a minúsculas
+//Contar la cantidad de caracteres
+//Clasificar el puntaje:
+//90 o más → Excelente
+//70 o más → Bueno
+//Menor a 70 → Insuficiente
+let frase = "" 
+
+
+//Ejercicio 18: Evaluación de tres notas con estado final
+//Crear una función que almacene el nombre de un estudiante y tres notas. Luego:
+//Calcular el promedio
+//Convertir el nombre a mayúsculas
+//Determinar el estado:
+//Promedio ≥ 6.0 → Destacado
+//Promedio ≥ 4.0 → Aprobado
+//Promedio < 4.0 → Reprobado
+function ejercicio18(){
+let name = "Rafael" 
+let n1 = "6.0"
+let n2 = "4.0"
+let n3 = "3.0"
+let promedio = (n1 + n2 + n3) /3
+if (nota >= 6.0) {
+alert (`El estudiante ${name.toUpperCase()} esta Destacado.
+\nPromedio: ${n1}
+\nLargo nombre: tiene ${name.length} caracteres`);
+}else if (nota >= 4.0) {
+alert(`El estudiante ${name.toUpperCase()}  esta Aprobado.
+\nPromedio: ${n2}
+\nLargo nombre: tiene ${name.length} caracteres.`);
+}else if (nota>= 3.0) {
+alert(`El estudiante ${name.toUpperCase()} esta Reprobado.
+\nPromedio: ${n3}
+\nLargo nombre: tiene ${name.length} caracteres. `);
+}
+}
+
+
+//Ejercicio 19: Clasificación de desempeño con más niveles
+//Crear una función que almacene el nombre de un estudiante y su promedio final. Luego:
+
+//Convertir el nombre a mayúsculas
+//Clasificar el rendimiento según:
+//≥ 6.5 → Sobresaliente
+//≥ 6.0 → Muy buen rendimiento
+//≥ 5.0 → Buen rendimiento
+//≥ 4.0 → Suficiente
+//< 4.0 → Insuficiente
+function ejercicios19(){
+let name = "Jake"
+let n1 = "6.5"
+let n2 = "6.0"
+let n3 = "5.0"
+let n4 = "4.0"
+let n5 = "4.0"
+let promedio = (n1 + n2 + n3 + n4 + n5) /5
+if (nota >= 6.5) {
+alert(`El estudiante ${name.toUpperCase()} tiene Sobresaliente
+\nPromedio: ${n1}
+\nLargo nombre: tiene ${name.length} caracteres.`)
+}else if (nota >= 6.0) {
+alert(`El estudiante ${name.toUpperCase()} tiene muy buen rendimiento
+\nPromedio: ${n2}
+\nLargo nombre: tiene ${name.length} caracteres.`)
+}else if (nota >= 5.0){
+alert(`El estudiante ${name.toUpperCase()} tiene buen rendimiento
+\nPromedio: ${n3}
+\nLargo nombre: tiene ${name.length} caracteres.`)
+}else if (nota >= 4.0){
+alert(`El estudiante ${name.toUpperCase()} tiene suficiente
+\nPromedio: ${n4}
+\nLargo nombre: tiene ${name.length} caracteres.`)
+}else if (nota <= 4.0){
+alert(`El estudiante ${name.toUpperCase()} tiene Insuficiente
+\nPromedio: ${n5}
+\nLargo nombre: tiene ${name.length} caracteres.`)
+}
+}
+//Ejercicio 20: Clasificación de producto con múltiples rangos de precio
+//Crear una función que almacene el nombre de un producto y su precio. Luego:
+
+//Convertir el producto a minúsculas
+//Clasificar el precio:
+//≥ 200.000 → Producto de lujo
+//≥ 100.000 → Producto premium
+//≥ 50.000 → Producto estándar
+//≥ 20.000 → Producto económico
+//< 20.000 → Producto básico
